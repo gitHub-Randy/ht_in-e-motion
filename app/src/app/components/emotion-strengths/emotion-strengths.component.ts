19ff288557813148e4314874de8862cb9c9ac885
TimvG <timvg1996@gmail.com>
2020-12-18 16:22:53 +0100

    added swiping

--- app/src/app/components/emotion-strengths/emotion-strengths.component.ts
+++ app/src/app/components/emotion-strengths/emotion-strengths.component.ts
@@ -6,19 +6,50 @@ import { choosenEmotions } from 'src/app/interfaces/chosenEmotions';
 import { MatDialog } from '@angular/material/dialog';
 import { DescriptionComponent } from './description/description.component';
 import { HelpPopUpComponent } from './help-pop-up/help-pop-up.component';
+import { trigger, keyframes, animate, transition, sequence, stagger, query } from '@angular/animations'
+import * as kf from '../emotion-selection/keyframes';
+import 'hammerjs';
 
 
 
 @Component({
   selector: 'app-emotion-strengths',
   templateUrl: './emotion-strengths.component.html',
-  styleUrls: ['./emotion-strengths.component.css']
+  styleUrls: ['./emotion-strengths.component.css'],
+  animations: [
+    trigger('emotionTrigger', [
+      transition('* => slideRight', [
+        query(':self', [
+          stagger(1000, [
+            animate(100, keyframes(kf.slideOutRight)),
+            animate(100, keyframes(kf.slideInLeft)),
+          ])
+        ], { optional: false })
+      ] ,
+
+      ),
+      transition('* => slideLeft', [
+        query(':self', [
+          stagger(1000, [
+            animate(100, keyframes(kf.slideOutLeft)),
+            animate(100, keyframes(kf.slideInRight)),
+        
+          ])
+        ], { optional: false })
+      ] ,
+
+      ),
+    ]),  
+  ]
 })
 export class EmotionStrengthsComponent implements OnInit, AfterViewInit{
 
   @ViewChild(HeaderComponent) childComponent: HeaderComponent;
   chosenEmotions: choosenEmotions[] = [];
   currentIndex: number = 0;
+  animationState: string;
+  shouldChange: boolean = false;
+
 
   constructor(private router: Router, private description: MatDialog, private help: MatDialog) {
     const navigation = this.router.getCurrentNavigation();
@@ -40,20 +71,28 @@ export class EmotionStrengthsComponent implements OnInit, AfterViewInit{
   }
 
   onLeft(){
+    this.changeSwipeControlColorToWhite()
     if(this.currentIndex > 0){
       this.currentIndex-=1;
     }else{
       this.currentIndex = this.chosenEmotions.length-1;
     }
+    this.changeSwipeControlColorToYellow()
+    this.startAnimation("slideRight")
+
   }
 
   onRight(){
+    this.changeSwipeControlColorToWhite()
+
     if(this.currentIndex < this.chosenEmotions.length - 1)
     {
       this.currentIndex += 1;
     }else{
       this.currentIndex = 0;
     }
+    this.changeSwipeControlColorToYellow()
+    this.startAnimation("slideLeft")
   }
 
 
@@ -89,6 +128,75 @@ export class EmotionStrengthsComponent implements OnInit, AfterViewInit{
 
   prevpage(){
     this.router.navigateByUrl('emotions');
+  }
+
+  changeSwipeControlColorToYellow() {
+    let parentDiv = document.getElementById("swipeControls");
+    let children = parentDiv.children as HTMLCollectionOf<HTMLElement>;
+    let iconToChange = children[this.currentIndex] ;
+    iconToChange.style.color = "#F8CB75";
+    iconToChange.style.backgroundColor  = "#F8CB75"
+  }
+
+  changeSwipeControlColorToWhite() {
+    let parentDiv = document.getElementById("swipeControls");
+    let children = parentDiv.children as HTMLCollectionOf<HTMLElement>;
+    let iconToChange = children[this.currentIndex];
+    iconToChange.style.color = "#FFFFFF";
+    iconToChange.style.backgroundColor = "#FFFFFF"
+  }
+
+  startAnimation(state) {
+    console.log(state);
+    if (!this.animationState) {
+      this.animationState = state
+    }
+
+  }
+
+
+
+  resetAnimationState() {
+    console.log(this.animationState)
+    if (this.animationState == "slideRight" && !this.shouldChange) {
+       this.onLeft();
+    } 
+    if (this.animationState == "slideLeft" && !this.shouldChange) {
+      
+       this.onRight();
+    }
+    console.log("reset")
+    this.animationState = '';
+    this.shouldChange = false;
+  }
+
+  goToIndex(index) {
+    let indexNew = index
+    console.log(indexNew)
+    if (indexNew > this.chosenEmotions.length-1) {
+      indexNew = 0
+    }
+    if (indexNew < 0) {
+      indexNew = this.chosenEmotions.length-1
+    }
+    console.log(indexNew)
+
+    let currentIndex = this.currentIndex;
+    if (indexNew > currentIndex ) {
+      this.shouldChange = true;
+      this.startAnimation("slideLeft")
+    } else if(indexNew < currentIndex ) {
+      this.shouldChange = true;
+      this.startAnimation("slideRight")
+    }
+   
+
+    this.changeSwipeControlColorToWhite();
+
+    this.currentIndex = indexNew;
+    
+    this.changeSwipeControlColorToYellow();
+
   }
 
 }
